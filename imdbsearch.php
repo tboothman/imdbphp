79660421452699b5fd59de8b06207103056e1bd8
izzy <izzysoft@users.noreply.github.com>
2007-01-22 17:57:11 +0000

    Initial revision

---
+++ imdbsearch.php
@@ -0,0 +1,26 @@
+<?
+
+require ("imdb.class.php");
+
+$search = new imdbsearch ();
+$search->setsearchname ($HTTP_GET_VARS["name"]);
+echo "<HTML><HEAD><TITLE>search</TITLE></HEAD><BODY>";
+$results = $search->results ();
+foreach ($results as $res) {
+
+     echo "<a href=imdb.php?mid=";
+     echo $res->imdbid();
+     echo ">";
+     echo $res->title();
+     echo "(".$res->year().")";
+     echo "</a> ";
+     echo " <a href=\"http://us.imdb.com/title/tt";
+     echo $res->imdbid();
+     echo "\">imdb page</a>";
+     echo "<br>\n";
+}
+
+echo "</BODY></HTML>";
+//echo $search->page;
+
+?>
