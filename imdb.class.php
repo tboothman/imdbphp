e6d8e905373467a36ab248e488c08476c779da5e
izzy <izzysoft@users.noreply.github.com>
2008-05-05 19:57:58 +0000

    * some optimization of tagline() and plotoutline()

--- imdb.class.php
+++ imdb.class.php
@@ -341,12 +341,8 @@
   function tagline () {
     if ($this->main_tagline == "") {
       if ($this->page["Title"] == "") $this->openpage ("Title");
-      if (@preg_match("/Tagline:\<\/h5\>\s*\n(.*?)\<\/div/ms",$this->page["Title"],$match)) {
-        if (@preg_match("/^(.*?) \<a class=\"tn15more/ms",$match[1],$hit)) {
-          $this->main_tagline = $hit[1];
-        } else {
-          $this->main_tagline = trim($match[1]);
-        }
+      if (@preg_match("/Tagline:\<\/h5\>\s*\n(.*?)(<\/div|<a class=\"tn15more)/ms",$this->page["Title"],$match)) {
+        $this->main_tagline = trim($match[1]);
       }
     }
     return $this->main_tagline;
@@ -377,13 +373,9 @@
   function plotoutline () {
     if ($this->main_plotoutline == "") {
       if ($this->page["Title"] == "") $this->openpage ("Title");
-      if (@preg_match("/Plot:\<\/h5\>\s*\n(.*?)<\/div/ms",$this->page["Title"],$match)) {
-        if (@preg_match("/^(.*?)<a class=\"tn15more/ms",$match[1],$hit))
-          $this->main_plotoutline = $hit[1];
-        else $this->main_plotoutline = trim($match[1]);
+      if (@preg_match("/Plot:\<\/h5\>\s*\n(.*?)(<\/div|\||<a class=\"tn15more)/ms",$this->page["Title"],$match)) {
+        $this->main_plotoutline = trim($match[1]);
       }
-      if (@preg_match("/^(.*)\|\s*$/",$this->main_plotoutline,$match)) $this->main_plotoutline = $match[1];
-      $this->main_plotoutline = trim($this->main_plotoutline);
     }
     return $this->main_plotoutline;
   }
