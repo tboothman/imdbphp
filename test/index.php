2fba3a0d9533f1e2253ef68641fcbc05745b0857
izzy <izzysoft@users.noreply.github.com>
2008-08-06 10:27:29 +0000

    + tests can now be restricted to either movie only, name only, or all (default)

--- test/index.php
+++ test/index.php
@@ -11,15 +11,29 @@
 
  /* $Id$ */
 
+#====================================================[ Output HTML Header ]===
 echo "<!DOCTYPE HTML PUBLIC '-//W3C//DTD HTML 4.01 Transitional//EN'>\n";
 echo "<HTML><HEAD>\n"
    . " <TITLE>IMDBAPI Checks</TITLE>\n"
    . " <STYLE TYPE='text/css'>body,td,th { font-size:12px; font-family:sans-serif; }</STYLE>\n"
    . "</HEAD><BODY>\n";
 
-require ("imdb.inc");
-require ("imdb_person.inc");
+#==========================================[ Define what should be tested ]===
+$check_movie = false;
+$check_name  = false;
+switch ($_REQUEST["check"]) {
+  case "movie" : $check_movie = true; break;
+  case "name"  : $check_name  = true; break;
+  default:
+    $check_movie = true;
+    $check_name  = true;
+}
 
+#=========================================================[ Run the tests ]===
+if ($check_movie) require ("imdb.inc");
+if ($check_name)  require ("imdb_person.inc");
+
+#===============================================[ Summary and HTML footer ]===
 $passed  = $methods - $failures;
 $percent = round(100*$passed/$methods)."%";
 echo "<H3>Test Results:</H3><UL><LI>Methods: $methods</LI><LI>Passed: $passed</LI><LI>Failures: $failures</LI><LI>Success: $percent</LI></UL>";
