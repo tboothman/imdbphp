6c3bf306b155670584220e49e21b453f6584502c
izzy <izzy@dde0ba4e-3f3c-0410-9201-8bb63b7b8dc3>
2009-09-29 18:58:14 +0000

    + the test pages now support a "cron-mode" passing "cron=1" at the URL

--- trunk/test/index.php
+++ trunk/test/index.php
@@ -11,20 +11,29 @@
 
  /* $Id$ */
 
+#===============================================[ optional URL Parameters ]===
+# check: only check the specified class (movie, name, charts, nowplay, trail)
+#        default: Check them all
+# skip : skip the test for the specified, comma-separated classes. Has
+#        priority over "check"
+# cron : enable cron-mode (cron=1) - i.e. output plain text, and on errors only
+# cache: enable (1|on|true) or disable (0|off|false) caching. Overrides config.
+
+require_once(dirname(__FILE__)."/helpers.inc");
 #=====================================[ Make sure it works out-of-the-box ]===
 $os = php_uname('s');
-$win = FALSE;
 $path = ini_get('include_path');
 if (strtolower(substr($os,0,3))=="win") ini_set('include_path',".;..;$path");
 else ini_set('include_path',".:..:$path");
 
 #====================================================[ Output HTML Header ]===
-echo "<!DOCTYPE HTML PUBLIC '-//W3C//DTD HTML 4.01 Transitional//EN'>\n";
-echo "<HTML><HEAD>\n"
-   . " <TITLE>IMDBAPI Checks</TITLE>\n"
-   . " <STYLE TYPE='text/css'>body,td,th { font-size:12px; font-family:sans-serif; }</STYLE>\n"
-   . "</HEAD><BODY>\n";
-
+if (!CRON) {
+  echo "<!DOCTYPE HTML PUBLIC '-//W3C//DTD HTML 4.01 Transitional//EN'>\n";
+  echo "<HTML><HEAD>\n"
+     . " <TITLE>IMDBAPI Checks</TITLE>\n"
+     . " <STYLE TYPE='text/css'>body,td,th { font-size:12px; font-family:sans-serif; }</STYLE>\n"
+     . "</HEAD><BODY>\n";
+}
 #==========================================[ Define what should be tested ]===
 $check_movie   = false;
 $check_name    = false;
@@ -44,6 +53,11 @@ switch ($_REQUEST["check"]) {
     $check_nowplay = true;
     $check_trail   = true;
 }
+if (!empty($_REQUEST["skip"])) {
+  $skips = explode(",",$_REQUEST["skip"]);
+  $segments = array("movie","name","charts","nowplay","trail");
+  foreach($skips as $skip) ${"check_$skip"} = FALSE;
+}
 
 #=========================================================[ Run the tests ]===
 if ($check_movie)   require ("imdb.inc");
@@ -55,7 +69,18 @@ if ($check_trail)   require ("imdb_trailers.inc");
 #===============================================[ Summary and HTML footer ]===
 $passed  = $methods - $failures;
 $percent = round(100*$passed/$methods)."%";
-echo "<H3>Test Results:</H3><UL><LI>Methods: $methods</LI><LI>Passed: $passed</LI><LI>Failures: $failures</LI><LI>Success: $percent</LI></UL>";
-if ($percent == "100%") echo "<b>Congratulations!</b> Looks like the complete API is working perfectly.";
-echo "</BODY></HTML>\n";
+if (CRON) {
+  if ($failures>0 || !empty($failed)) {
+    echo "Some methods failed the automated tests:\n"
+       . "========================================\n\n";
+    foreach ($failed as $fail) echo "* $fail\n";
+    echo "\nOverall results:\n\n"
+       . "Success: $passed/$methods ($percent)\n";
+  }
+} else {
+  headline("Test Results:");
+  raw("<UL><LI>Methods: $methods</LI><LI>Passed: $passed</LI><LI>Failures: $failures</LI><LI>Success: $percent</LI></UL>\n");
+  if ($percent == "100%") raw("<b>Congratulations!</b> Looks like the complete API is working perfectly.");
+  raw("</BODY></HTML>\n");
+}
 ?>
