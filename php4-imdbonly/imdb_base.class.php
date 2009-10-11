6c5309c68f6724984707eccdd861720632c99684
izzy <izzy@dde0ba4e-3f3c-0410-9201-8bb63b7b8dc3>
2009-10-11 21:09:00 +0000

    * backporting r239 to the PHP4 branch (wrong version number)

--- php4-imdbonly/imdb_base.class.php
+++ php4-imdbonly/imdb_base.class.php
@@ -39,7 +39,7 @@
    * @class imdb_base
    * @attribute string lastServerResponse
    */
-  var $version = '1.1.6';
+  var $version = '1.2.0';
 
  #---------------------------------------------------------[ Debug helpers ]---
   function debug_scalar($scalar) {
