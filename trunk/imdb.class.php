6668b611c4f261065dbddee18355ff0cc2580c00
izzy <izzy@dde0ba4e-3f3c-0410-9201-8bb63b7b8dc3>
2010-09-28 17:38:12 +0000

    ! more site changes - fixes backported from trunk (photos, countries, languages)

--- trunk/imdb.class.php
+++ trunk/imdb.class.php
@@ -282,11 +282,12 @@
   public function languages() {
    if (empty($this->langs)) {
     if ($this->page["Title"] == "") $this->openpage ("Title");
-    if (preg_match_all("/\/Sections\/Languages\/.*?>\s*(.*?)\s*</m",$this->page["Title"],$matches))
+    if (preg_match_all('!<a href="/language/.*?">\s*(.*?)\s*<!m',$this->page["Title"],$matches))
       $this->langs = $matches[1];
    }
    return $this->langs;
   }
+
 
  #--------------------------------------------------------------[ Genre(s) ]---
   /** Get the movies main genre
