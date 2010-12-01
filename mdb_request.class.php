cde77b831f06fb3ef1a632ae0eab4770ece2f0e4
izzy <izzysoft@users.noreply.github.com>
2010-12-01 16:20:49 +0000

    * some more work on the photo stuff. This should hopefully solve ticket:192 and ticket:197 and whatever in this regard

--- mdb_request.class.php
+++ mdb_request.class.php
@@ -55,9 +55,13 @@ if ( isset($PEAR) && $PEAR ) { // Use the HTTP_Request class from the PEAR proje
       $this->urltoopen = $url;
       $iconf = new mdb_config;
       if (!empty($force_agent)) $iconf->force_agent = $force_agent;
-      switch (strtolower($trigger_referer)) {
-        case 'true' : $iconf->trigger_referer = TRUE; break;
-        case 'false': $iconf->trigger_referer = FALSE; break;
+      if ($trigger_referer !== '' && is_bool($trigger_referer)) {
+        $iconf->trigger_referer = $trigger_referer;
+      } else {
+        switch (strtolower($trigger_referer)) {
+          case 'true' : $iconf->trigger_referer = TRUE; break;
+          case 'false': $iconf->trigger_referer = FALSE; break;
+        }
       }
       if ($iconf->trigger_referer) {
         if ( substr(get_class($this),0,4)=="imdb" ) $this->addHeaderLine('Referer','http://' . $this->imdbsite . '/');
