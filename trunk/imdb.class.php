f1bcdee7a4483a8ec1529d70ae37a60d316a0201
izzy <izzy@dde0ba4e-3f3c-0410-9201-8bb63b7b8dc3>
2011-12-17 23:28:20 +0000

    * sometimes imdb::genres() did not return all available genres (see e.g. ticket:261)

--- trunk/imdb.class.php
+++ trunk/imdb.class.php
@@ -388,8 +388,8 @@
   public function genres() {
     if (empty($this->moviegenres)) {
       if ($this->page["Title"] == "") $this->openpage ("Title");
-      if (preg_match_all("@<a href\=\"/genre/[\w\-]+\"\>(.*?)\</a>@",$this->page["Title"],$matches)) {
-        $this->moviegenres = $matches[1];
+      if (preg_match_all("@<a href\=\"/genre/[\w\-]+\"[^>]*\>(.*?)\</a>@",$this->page["Title"],$matches)) {
+        $this->moviegenres = array_unique($matches[1]);
       } elseif (preg_match('!<div class="infobar">(.*?)</div>!ims',$this->page['Title'],$match)) {
         if (preg_match_all('!href="/genre/.*?"\s*>(.*?)<!ims',$match[1],$matches)) {
           $this->moviegenres = $matches[1];
