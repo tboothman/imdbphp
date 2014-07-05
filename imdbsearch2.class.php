521e2f6d46de4275f54dbee40f522101d44484ad
DJMcTom <tboothman@users.noreply.github.com>
2014-07-05 17:45:41 +0000

    Add all the TV types I can find on imdb

--- imdbsearch2.class.php
+++ imdbsearch2.class.php
@@ -5,18 +5,21 @@ require_once (dirname(__FILE__) . "/imdb.class.php");
 class imdbsearch2 extends mdb_base {
 
   const MOVIE = 'Movie';
-  const TV = 'TV Series';
+  const TV_SERIES = 'TV Series';
   const TV_EPISODE = 'TV Episode';
+  const TV_MINI_SERIES = 'TV Mini-Series';
+  const TV_MOVIE = 'TV Movie';
+  const TV_SPECIAL = 'TV Special';
+  const TV_SHORT = 'TV Short';
   const GAME = 'Video Game';
   const VIDEO = 'Video';
   const SHORT = 'Short';
-  const SPECIAL = 'Special'; //@TODO: What's a special? find one
 
   /**
    * Search IMDb for titles matching $searchTerms
    * @param type $searchTerms
    * @param array $wantedTypes *optional* imdb types that should be returned. Defaults to returning all types.
-   *                            The class constants FILM,TV etc should be used e.g. [imdbsearch::FILM, imdbsearch::TV]
+   *                            The class constants MOVIE,GAME etc should be used e.g. [imdbsearch::MOVIE, imdbsearch::TV_SERIES]
    * @param int $maxResults *optional* The maximum number of results to retrieve from IMDB. 0 for unlimited. Defaults to mdb_config::$maxresults
    * @return array array of imdb objects
    */
@@ -87,7 +90,7 @@ class imdbsearch2 extends mdb_base {
     $string = strtoupper($string);
 
     if (strpos($string, 'TV SERIES') !== FALSE) {
-      return self::TV;
+      return self::TV_SERIES;
     } elseif (strpos($string, 'TV EPISODE') !== FALSE) {
       return self::TV_EPISODE;
     } elseif (strpos($string, 'VIDEO GAME') !== FALSE) {
@@ -96,8 +99,14 @@ class imdbsearch2 extends mdb_base {
       return self::VIDEO;
     } elseif (strpos($string, '(SHORT)') !== FALSE) {
       return self::SHORT;
-    } elseif (strpos($string, 'SPECIAL)') !== FALSE) {
-      return self::SPECIAL;
+    } elseif (strpos($string, 'TV MINI-SERIES)') !== FALSE) {
+      return self::TV_MINI_SERIES;
+    } elseif (strpos($string, 'TV MOVIE)') !== FALSE) {
+      return self::TV_MOVIE;
+    } elseif (strpos($string, 'TV SPECIAL)') !== FALSE) {
+      return self::TV_SPECIAL;
+    } elseif (strpos($string, 'TV SHORT)') !== FALSE) {
+      return self::TV_SHORT;
     } else {
       return self::MOVIE;
     }
@@ -110,6 +119,7 @@ class imdbsearch2 extends mdb_base {
   protected $searchTerms;
   protected $episode_search = false;
   /**
+   * DEPRICATED
    * Search for episodes or movies
    * @param boolean enabled TRUE: Search for episodes; FALSE: Search for movies (default)
    * @deprecated since version 2.2.4
@@ -120,6 +130,7 @@ class imdbsearch2 extends mdb_base {
   }
 
   /**
+   * DEPRICATED
    * Set the name (title) to search for
    * @param string searchstring what to search for - (part of) the movie name
    * @deprecated since version 2.2.4
@@ -130,6 +141,7 @@ class imdbsearch2 extends mdb_base {
   }
 
   /**
+   * DEPRICATED
    * Perform the search
    * @param optional string URL Replace search URL by your own (Default: empty string)
    * @param optional boolean series whether to include TV series in search results (default: TRUE)
@@ -144,9 +156,9 @@ class imdbsearch2 extends mdb_base {
   public function results($url = "", $series = TRUE, $s_episodes = TRUE, $s_games = TRUE, $s_video = TRUE, $s_short = TRUE, $s_special = TRUE) {
     trigger_error('imdbsearch has a new interface. Please use imdbsearch::search instead', E_USER_DEPRECATED);
 
-    $searchTypes = [self::MOVIE];
+    $searchTypes = [self::MOVIE, self::TV_MINI_SERIES, self::TV_MOVIE, self::TV_SHORT];
     if ($series) {
-      $searchTypes[] = self::TV;
+      $searchTypes[] = self::TV_SERIES;
     }
     if ($s_episodes) {
       $searchTypes[] = self::TV_EPISODE;
@@ -161,7 +173,7 @@ class imdbsearch2 extends mdb_base {
       $searchTypes[] = self::SHORT;
     }
     if ($s_special) {
-      $searchTypes[] = self::SPECIAL;
+      $searchTypes[] = self::TV_SPECIAL;
     }
     if ($this->episode_search) {
       // yea, this looks stupid .. but it's how the old one worked. This made a different call to imdb for episodes only
