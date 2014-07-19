9ce71d8889c7f4fae0dd25017bf0c0f0920d8447
DJMcTom <DJMcTom@dde0ba4e-3f3c-0410-9201-8bb63b7b8dc3>
2014-07-19 19:40:46 +0000

    Replace search with better version. Moved person search into its own file.

--- trunk/mdb_base.class.php
+++ trunk/mdb_base.class.php
@@ -45,7 +45,7 @@ define('FULL_ACCESS',9);
  * @version $Revision$ $Date$
  */
 class mdb_base extends mdb_config {
-  public $version = '2.2.3';
+  public $version = '2.2.4';
 
   /**
    * The last HTTP status code from the IMDB server
