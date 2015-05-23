d80e64c1af0442e262f186dfa5f80ba1204b8bea
Tom Boothman <tboothman@gmail.com>
2015-05-23 18:48:05 +0100

    Merge budget into Title

--- src/Imdb/Title.php
+++ src/Imdb/Title.php
@@ -90,6 +90,12 @@ class Title extends MdbBase {
   protected $parental_guide = array();
   protected $official_sites = array();
   protected $locations = array();
+  protected $budget = null;
+  protected $openingWeekend = array();
+  protected $gross = array();
+  protected $weekendGross = array();
+  protected $admissions = array();
+  protected $filmingDates = array();
 
   /**
    * Create an imdb object populated with id, title, year, and movie type
@@ -222,6 +228,12 @@ class Title extends MdbBase {
    $this->parental_guide = array();
    $this->official_sites = array();
    $this->locations = array();
+   $this->budget = null;
+   $this->openingWeekend = array();
+   $this->gross = array();
+   $this->weekendGross = array();
+   $this->admissions = array();
+   $this->filmingDates = array();
   }
 
  #-------------------------------------------------------------[ Open Page ]---
@@ -254,6 +266,7 @@ class Title extends MdbBase {
     case "Keywords"       : $urlname="/keywords"; break;
     case "Awards"      : $urlname="/awards"; break;
     case "Locations"   : $urlname="/locations"; break;
+    case "BoxOffice"   : $urlname = "/business"; break;
     default            :
       if ( preg_match('!^Episodes-(\d+)$!',$wt,$match) ) {
         $urlname = '/episodes?season='.$match[1];
@@ -2204,7 +2217,6 @@ class Title extends MdbBase {
   #========================================================[ /awards page ]===
   #--------------------------------------------------------------[ Awards ]---
   /** Get all awards this title was nominated for or won
-   * @method awards
    * @param boolean $compat whether stay backward compatible to the original format of Qvist. Default: TRUE
    * @return array awards array[festivalName]['entries'][0..n] of array[year,won,category,award,people[],comment,outcome]
    * e.g.
@@ -2281,6 +2293,362 @@ class Title extends MdbBase {
       }
     }
     return $this->awards;
+  }
+
+  /* Get budget
+  * @param string budg
+  * @return int|null null on failure
+  * @brief Assuming budget is estimated, and in american dollar
+  * @see IMDB page / (TitlePage)
+  */
+ protected function get_budget($budg){
+     // Tries to get a single entry
+    if (@preg_match("!(.*?)\s*\(estimated\)!ims", $budg, $opWe)) {
+      $result = $opWe[1];
+      return intval(substr(str_replace(",", "", $result), 1));
+    } else {
+      return null;
+    }
+ } // End of get_budget
+
+ /* Get budget
+  * @return int|null null on failure / no data
+  * @brief Assuming budget is estimated, and in american dollar
+  * @see IMDB page / (TitlePage)
+  */
+  public function budget() {
+    if (empty($this->budget)) {
+      $page = $this->getPage("BoxOffice");
+      if (@preg_match("!<h5>Budget</h5>\s*\n*(.*?)(<br/>\n*)*<h5!ims", $page, $bud)) { // Opening Weekend
+        $budget = $bud[1];
+      } else {
+        return null;
+      }
+      $this->budget = $this->get_budget($budget);
+    }
+    return $this->budget;
+  }
+
+  #-------------------------------------------------[ Openingbudget ]---
+ /** Get opening weekend budget
+  * @param ref string listOpening
+  * @return array[0..n] of array[value,country,date,nbScreens]
+  * @see IMDB page
+  */
+ protected function get_openingWeekend(&$listOpening){
+   $result = array();
+   $temp = $listOpening;
+
+   $i = 0;
+   while($temp != NULL){
+     // Tries to get a single entry
+     if (@preg_match("!(.*?)<br/>!ims",$temp,$opWe))
+     $entry = $opWe[1];
+
+     // Tries to extract the value
+     if (@preg_match("!(.*?)\(!ims",$opWe[1],$value))
+       $opWe[1] = str_replace($value,"",$opWe[1]);
+
+     // Tries to extract the country
+     if (@preg_match("!(.*?)\)\s*!ims",$opWe[1],$country))
+       $opWe[1] = str_replace($country,"",$opWe[1]);
+
+     // Tries to extract the date
+     if (@preg_match("!\((.*?)\)\s*!ims",$opWe[1],$date))
+       if (@preg_match("!<a href=\"/date/(.*?)/\">!ims",$date[1],$dayMonth))
+         if (@preg_match("!<a href=\"/year/(.*?)/\">!ims",$date[1],$year))
+           $dateValue = $year[1].'-'.$dayMonth[1];
+     $opWe[1] = str_replace($date,"",$opWe[1]);
+
+     // Tries to extract the number of screens
+     if (@preg_match("!\((.*?)\)\s*!ims",$opWe[1],$nbScreen))
+       $opWe[1] = str_replace($nbScreen,"",$opWe[1]);
+
+     // Parse the results in an array
+     $result[$i] = array(
+       'value'     => trim($value[1]),
+       'country'   => $country[1],
+       'date'      => $dateValue,
+       'nbScreens' => intval(str_replace(",","",$nbScreen[1]))
+     );
+
+     // Remove the entry from the list of entries
+     if (@preg_match("!<br/>(.*?)$!ims",$temp,$temp))
+       $temp = $temp[1];
+
+     $i++;
+   }
+   return $result;
+ } // End of get_openingWeekend
+
+
+ /** Opening weekend budget
+   * @return array[0..n] of array[value,country,date,nbScreens]
+   * @see IMDB page
+   * @TODO fix 'value' field .. "&#163;3,384,948" isn't good enough
+   */
+  public function openingWeekend() {
+    if (empty($this->openingWeekend)) {
+      $page = $this->getPage("BoxOffice");
+      if (@preg_match("!<h5>Opening Weekend</h5>\n*(.*?)<br/>\n*<h5!ims", $page, $opWe)) // Opening Weekend
+        $openingWeekend = $opWe[1];
+      $this->openingWeekend = $this->get_openingWeekend($openingWeekend);
+    }
+    return $this->openingWeekend;
+  }
+
+ #-------------------------------------------------[ Gross ]---
+ /** Get gross budget
+  * @param ref string listGross
+  * @return array[0..n] of array[value,country,date]
+  * @see IMDB page / (TitlePage)
+  */
+ protected function get_gross(&$listGross) {
+   $result = array();
+   $temp = $listGross;
+   $i = 0;
+   while($temp != NULL){
+     // Tries to get a single entry
+     if (@preg_match("!(.*?)<br/>!ims",$temp,$gr))
+       $entry = $gr[1];
+       //echo 'ici'.$entry.'ici';
+
+     // Tries to extract the value
+     if (@preg_match("!(.*?)\(!ims",$gr[1],$value))
+       $gr[1] = str_replace($value,"",$gr[1]);
+
+     // Tries to extract the country
+     if (@preg_match("!(.*?)\)\s*!ims",$gr[1],$country))
+       $gr[1] = str_replace($country,"",$gr[1]);
+
+     // Tries to extract the date
+     if (@preg_match("!\((.*?)\)\s*!ims",$gr[1],$date))
+       if (@preg_match("!<a href=\"/date/(.*?)/\">!ims",$date[1],$dayMonth))
+         if (@preg_match("!<a href=\"/year/(.*?)/\">!ims",$date[1],$year))
+           $dateValue = $year[1].'-'.$dayMonth[1];
+     $gr[1] = str_replace($date,"",$gr[1]);
+
+     // Parse the results in an array
+     $result[$i] = array(
+       'value'     => trim($value[1]),
+       'country'   => $country[1],
+       'date'      => $dateValue,
+     );
+
+     // Remove the entry from the list of entries
+     if (@preg_match("!<br/>(.*?)$!ims",$temp,$temp))
+       $temp = $temp[1];
+
+     $i++;
+   }
+   return $result;
+ } // End of get_gross
+
+
+ /** Get gross budget
+   * @return array[0..n] of array[value,country,date]
+   * @see IMDB page / (TitlePage)
+   * @TODO fix 'value' field .. "&#163;3,384,948" isn't good enough
+   */
+  public function gross() {
+    if (empty($this->gross)) {
+      $page = $this->getPage("BoxOffice");
+      if (@preg_match("!<h5>Gross</h5>\n*(.*?)<br/>\n*<h5!ims", $page, $gr)) // Gross
+        $gross = $gr[1];
+      $this->gross = $this->get_gross($gross);
+    }
+    return $this->gross;
+  }
+
+
+ #-------------------------------------------------[ Weekend Gross ]---
+ /** Get weekend gross budget
+  * @param ref string listweekendGross
+  * @return array[0..n] of array[value,country,date,nbScreens]
+  * @see IMDB page / (TitlePage)
+  */
+ protected function get_weekendGross(&$listweekendGross){
+   $result = array();
+   $temp = $listweekendGross;
+   $i = 0;
+
+   while($temp != NULL){
+     // Tries to get a single entry
+     if (@preg_match("!(.*?)<br/>!ims",$temp,$weGr))
+       $entry = $weGr[1];
+
+     // Tries to extract the value
+     if (@preg_match("!(.*?)\(!ims",$weGr[1],$value))
+       $weGr[1] = str_replace($value,"",$weGr[1]);
+
+     // Tries to extract the country
+     if (@preg_match("!(.*?)\)\s*!ims",$weGr[1],$country))
+       $weGr[1] = str_replace($country,"",$weGr[1]);
+
+     // Tries to extract the date
+     if (@preg_match("!\((.*?)\)\s*!ims",$weGr[1],$date))
+       if (@preg_match("!<a href=\"/date/(.*?)/\">!ims",$date[1],$dayMonth))
+         if (@preg_match("!<a href=\"/year/(.*?)/\">!ims",$date[1],$year))
+           $dateValue = $year[1].'-'.$dayMonth[1];
+     $weGr[1] = str_replace($date,"",$weGr[1]);
+
+     // Tries to extract the number of screens
+     if (@preg_match("!\((.*?)\)\s*!ims",$weGr[1],$nbScreen))
+       $weGr[1] = str_replace($nbScreen,"",$weGr[1]);
+
+     // Parse the results in an array
+     $result[$i] = array(
+       'value'     => trim($value[1]),
+       'country'   => $country[1],
+       'date'      => $dateValue,
+       'nbScreens' => intval(str_replace(",","",$nbScreen[1]))
+     );
+
+     // Remove the entry from the list of entries
+     if (@preg_match("!<br/>(.*?)$!ims",$temp,$temp))
+       $temp = $temp[1];
+
+     $i++;
+   }
+   return $result;
+ } // End of get_weekendGross
+
+
+ /** Get weekend gross budget
+   * @return array[0..n] of array[value,country,date,nbScreen]
+   * @see IMDB page / (TitlePage)
+   */
+  public function weekendGross() {
+    if (empty($this->weekendGross)) {
+      $page = $this->getPage("BoxOffice");
+      if (@preg_match("!<h5>Weekend Gross</h5>\n*(.*?)<br/>\n*<h5!ims", $page, $weGr)) // Weekend Gross
+        $weekendGross = $weGr[1];
+      $this->weekendGross = $this->get_weekendGross($weekendGross);
+    }
+    return $this->weekendGross;
+  }
+
+  #-------------------------------------------------[ Admissions ]---
+ /** Get admissions budget
+  * @param ref string listAdmissions
+  * @return array[0..n] of array[value,country,date]
+  * @see IMDB page / (TitlePage)
+  */
+ protected function get_admissions(&$listAdmissions) {
+   $result = array();
+   $temp = $listAdmissions;
+   $i = 0;
+
+   while($temp != NULL){
+     // Tries to get a single entry
+     if (@preg_match("!(.*?)<br/>!ims",$temp,$adm))
+       $entry = $adm[1];
+
+     // Tries to extract the value
+     if (@preg_match("!(.*?)\(!ims",$adm[1],$value))
+       $adm[1] = str_replace($value,"",$adm[1]);
+
+     // Tries to extract the country
+     if (@preg_match("!(.*?)\)\s*!ims",$adm[1],$country))
+       $adm[1] = str_replace($country,"",$adm[1]);
+
+     // Tries to extract the date
+     if (@preg_match("!\((.*?)\)\s*!ims",$adm[1],$date))
+       if (@preg_match("!<a href=\"/date/(.*?)/\">!ims",$date[1],$dayMonth))
+         if (@preg_match("!<a href=\"/year/(.*?)/\">!ims",$date[1],$year))
+           $dateValue = $year[1].'-'.$dayMonth[1];
+     $adm[1] = str_replace($date,"",$adm[1]);
+
+     // Parse the results in an array
+     $result[$i] = array(
+       'value'     => intval(str_replace(",","",$value[1])),
+       'country'   => $country[1],
+       'date'      => $dateValue,
+     );
+
+     // Remove the entry from the list of entries
+     if (@preg_match("!<br/>(.*?)$!ims",$temp,$temp))
+       $temp = $temp[1];
+
+     $i++;
+   }
+   return $result;
+ } // End of get_admissions
+
+
+ /** Get admissions budget
+   * @return array[0..n] of array[value,country,date]
+   * @see IMDB page / (business)
+   */
+  public function admissions() {
+    if (empty($this->admissions)) {
+      $page = $this->getPage("BoxOffice");
+      if (@preg_match("!<h5>Admissions</h5>\n*(.*?)<br/>\n*<h5!ims", $page, $weGr)) // Admissions
+        $admissions = $weGr[1];
+      $this->admissions = $this->get_admissions($admissions);
+    }
+    return $this->admissions;
+  }
+
+  #-------------------------------------------------[ Filming Dates ]---
+ /** Get filming dates
+  * @param ref string listFilmingDates
+  * @return array[0..n] of array[beginning,end]
+  * Time format : YYYY-MM-DD
+  * @see IMDB page / (TitlePage)
+  */
+ protected function get_filmingDates($listFilmingDates){
+   $temp = $listFilmingDates;
+
+   // Tries to get the beginning
+   if (@preg_match("!(.*?)&nbsp;-!ims",$temp,$beginning))
+
+     if (@preg_match("#[A-Z0-9]#",$beginning[1])) { // Check if there is a date
+       if (@preg_match("!<a(.*?)&nbsp;-!ims",$temp)) { // Check if there  is a linked date
+         if (@preg_match("!<a href=\"/date/(.*?)/\">!ims",$beginning[1],$dayMonthB))
+           if (@preg_match("!<a href=\"/year/(.*?)/\">!ims",$beginning[1],$yearB))
+             $beginningDate = $yearB[1].'-'.$dayMonthB[1];
+       } else if (@preg_match("!(.*?)&nbsp;-!ims",$temp,$beginning)) {
+         $beginningDate = date('Y-m-d', strtotime($beginning[1]));
+       }
+     }
+
+   // Tries to get the end
+   if (@preg_match("!-&nbsp;(.*?)$!ims",$temp,$end))
+
+     if (@preg_match("#[A-Z0-9]#",$end[1])) { // Check if there is a date
+       if (@preg_match("!-&nbsp;<a(.*?)!ims",$temp)) { // Check if there  is a linked date
+         if (@preg_match("!<a href=\"/date/(.*?)/\">!ims",$end[1],$dayMonthE))
+           if (@preg_match("!<a href=\"/year/(.*?)/\">!ims",$end[1],$yearE))
+             $endDate = $yearE[1].'-'.$dayMonthE[1];
+       } else if (@preg_match("!-&nbsp;(.*?)$!ims",$temp,$end)) {
+         $endDate = date('Y-m-d', strtotime($end[1]));
+       }
+     }
+
+   // Parse the results in an array
+   $result = array(
+       'beginning' => $beginningDate,
+       'end'       => $endDate
+   );
+
+   return $result;
+ }
+
+
+ /** Get filming dates
+   * @return array[beginning, end]
+   * Time format : YYYY-MM-DD
+   * @see IMDB page / (TitlePage)
+   */
+  public function filmingDates() {
+    if (empty($this->filmingDates)) {
+      $page = $this->getPage("BoxOffice");
+      if (@preg_match("!<h5>Filming Dates</h5>\s*\n*(.*?)(<br/>\n*)*<h5!ims", $page, $filDates)) // Filming Dates
+        $filmingDates = $filDates[1];
+      $this->filmingDates = $this->get_filmingDates($filmingDates);
+    }
+    return $this->filmingDates;
   }
 
 }
--- tests/TitleTest.php
+++ tests/TitleTest.php
@@ -1017,6 +1017,88 @@ class imdbTest extends PHPUnit_Framework_TestCase {
       $this->assertEquals('Don Davis', $firstEntry['people']['0204485']);
       $this->assertEquals('Won', $firstEntry['outcome']);
     }
+
+  public function test_budget() {
+    $budget = $this->getImdb();
+    $this->assertEquals(63000000, $budget->budget());
+  }
+
+  public function test_openingWeekend_multiple() {
+    $budget = $this->getImdb();
+    $openingWeekend = $budget->openingWeekend();
+    $this->assertInternalType('array', $openingWeekend);
+
+    $firstItem = $openingWeekend[0];
+    $this->assertEquals('$27,788,331', $firstItem['value']);
+    $this->assertEquals('USA', $firstItem['country']);
+    $this->assertEquals('1999-04-04', $firstItem['date']);
+    $this->assertEquals(2849, $firstItem['nbScreens']);
+
+    $secondItem = $openingWeekend[1];
+    $this->assertEquals('&#163;3,384,948', $secondItem['value']);
+    $this->assertEquals('UK', $secondItem['country']);
+    $this->assertEquals('1999-06-13', $secondItem['date']);
+    $this->assertEquals(361, $secondItem['nbScreens']);
+  }
+
+  public function test_gross_multiple() {
+    $budget = $this->getImdb();
+    $gross = $budget->gross();
+    $this->assertInternalType('array', $gross);
+
+    $firstItem = $gross[0];
+    $this->assertEquals('$171,479,930', $firstItem['value']);
+    $this->assertEquals('USA', $firstItem['country']);
+    $this->assertEquals('1999-09-26', $firstItem['date']);
+
+    $secondItem = $gross[26];
+    $this->assertEquals('&#163;16,918,842', $secondItem['value']);
+    $this->assertEquals('UK', $secondItem['country']);
+    $this->assertEquals('1999-08-29', $secondItem['date']);
+  }
+
+  public function test_weekendGross_multiple() {
+    $budget = $this->getImdb();
+    $weekendGross = $budget->weekendGross();
+    $this->assertInternalType('array', $weekendGross);
+
+    $firstItem = $weekendGross[0];
+    $this->assertEquals('$1,011,566', $firstItem['value']);
+    $this->assertEquals('USA', $firstItem['country']);
+    $this->assertEquals('1999-06-27', $firstItem['date']);
+    $this->assertEquals(1139, $firstItem['nbScreens']);
+
+    $secondItem = $weekendGross[13];
+    $this->assertEquals('&#163;63,166', $secondItem['value']);
+    $this->assertEquals('UK', $secondItem['country']);
+    $this->assertEquals('1999-08-29', $secondItem['date']);
+    $this->assertEquals(87, $secondItem['nbScreens']);
+  }
+
+  public function test_admissions_multiple() {
+    $budget = $this->getImdb();
+    $admissions = $budget->admissions();
+    $this->assertInternalType('array', $admissions);
+
+    $firstItem = $admissions[0];
+    $this->assertEquals(178659, $firstItem['value']);
+    $this->assertEquals('Germany', $firstItem['country']);
+    $this->assertEquals('2003-05-25', $firstItem['date']);
+
+    $secondItem = $admissions[1];
+    $this->assertEquals(3194163, $secondItem['value']);
+    $this->assertEquals('Germany', $secondItem['country']);
+    $this->assertEquals('1999-07-18', $secondItem['date']);
+  }
+
+  public function test_filmingDates() {
+    $budget = $this->getImdb();
+
+    $filmingDates = $budget->filmingDates();
+    $this->assertInternalType('array', $filmingDates);
+    $this->assertEquals('1998-03-14', $filmingDates['beginning']);
+    $this->assertEquals('1998-09-01', $filmingDates['end']);
+  }
     
     /**
      * Create an imdb object that uses cached pages
