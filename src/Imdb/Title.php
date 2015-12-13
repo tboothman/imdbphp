18bb394d461e3a416eb599ea7bc5651ab5f4d3aa
Alexander Schulze <github@lxhome.de>
2015-12-13 13:50:59 +0100

    Fix finding poster image on title page

--- src/Imdb/Title.php
+++ src/Imdb/Title.php
@@ -867,7 +867,7 @@ class Title extends MdbBase {
    */
   protected function thumbphoto() {
     $this->getPage("Title");
-    preg_match('!id="img_primary">.*?<img [^>]+src="(.+?)".*?<td id="overview-top"!ims',$this->page["Title"],$match);
+    preg_match('!<img [^>]+src="([^"]+)"[^>]+itemprop="image" />!ims',$this->page["Title"],$match);
     if (empty($match[1])) return FALSE;
     $this->main_thumb = $match[1];
     if ( preg_match('|(.*\._V1).*|iUs',$match[1],$mo) ) {
