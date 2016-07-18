87379af64ca5e7a40f95508e337afb44289d0643
Tom Boothman <tboothman@users.noreply.github.com>
2016-07-18 21:31:44 +0100

    Fix weird loop for soundtracks in demo site

--- demo/movie.php
+++ demo/movie.php
@@ -385,10 +385,10 @@ if (isset ($_GET["mid"]) && preg_match('/^[0-9]+$/',$_GET["mid"])) {
     echo '<tr><td valign=top><b>Soundtracks:</b></td><td>';
     echo "There are $gc soundtracks listed - like these:<br>";
     echo "<table align='center' border='1' style='border-collapse:collapse;background-color:#ddd;'><tr><th style='background-color:#07f;'>Soundtrack</th><th style='background-color:#07f;'>Credit 1</th><th style='background-color:#07f;'>Credit 2</th></tr>";
-    for ($i=0;$i<5;++$i) {
-      if (isset($soundtracks[$i]["credits"][0])) $credit1 = preg_replace("/http\:\/\/".str_replace(".","\.",$movie->imdbsite)."\/name\/nm(\d{7})\//","person.php?mid=\\1",$soundtracks[$i]["credits"][0]['credit_to'])." (".$soundtracks[$i]["credits"][0]['desc'].")"; else $credit1 = '';
-      if (isset($soundtracks[$i]["credits"][1])) $credit2 = preg_replace("/http\:\/\/".str_replace(".","\.",$movie->imdbsite)."\/name\/nm(\d{7})\//","person.php?mid=\\1",$soundtracks[$i]["credits"][1]['credit_to'])." (".$soundtracks[$i]["credits"][1]['desc'].")"; else $credit2 = '';
-      echo "<tr><td>".$soundtracks[$i]["soundtrack"]."</td><td>$credit1</td><td>$credit2</td></tr>";
+    foreach ($soundtracks as $soundtrack) {
+      if (isset($soundtrack["credits"][0])) $credit1 = preg_replace("/http\:\/\/".str_replace(".","\.",$movie->imdbsite)."\/name\/nm(\d{7})\//","person.php?mid=\\1",$soundtrack["credits"][0]['credit_to'])." (".$soundtrack["credits"][0]['desc'].")"; else $credit1 = '';
+      if (isset($soundtrack["credits"][1])) $credit2 = preg_replace("/http\:\/\/".str_replace(".","\.",$movie->imdbsite)."\/name\/nm(\d{7})\//","person.php?mid=\\1",$soundtrack["credits"][1]['credit_to'])." (".$soundtrack["credits"][1]['desc'].")"; else $credit2 = '';
+      echo "<tr><td>".$soundtrack["soundtrack"]."</td><td>$credit1</td><td>$credit2</td></tr>";
     }
     echo "</table></td></tr>\n";
   }
