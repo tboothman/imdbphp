3fd2d598c5e270ad5d1454e0e72d971745b620d6
Tom Boothman <tboothman@users.noreply.github.com>
2017-10-30 20:50:21 +0000

    Fix test

--- tests/TitleSearchTest.php
+++ tests/TitleSearchTest.php
@@ -104,7 +104,7 @@ class TitleSearchTest extends PHPUnit_Framework_TestCase {
     $this->assertInternalType('array', $results);
 
     /* @var $firstResult Title */
-    $firstResult = $results[1];
+    $firstResult = $results[2];
     $this->assertInstanceOf('\Imdb\Title', $firstResult);
     $this->assertEquals("2224026", $firstResult->imdbid());
     $this->assertEquals("Home", $firstResult->title());
