5870b7b9287b00caa31b3562dbc18532bba9b3cb
MOHAMMAD <13052531+M0H4MM4D0UTIS@users.noreply.github.com>
2019-09-03 01:26:58 +0430

    fix title end year tests
    fix title end year tests

--- tests/TitleTest.php
+++ tests/TitleTest.php
@@ -235,7 +235,7 @@ class imdb_titleTest extends PHPUnit_Framework_TestCase
     public function testYearspan_for_a_tv_show_that_havent_ended()
     {
         $imdb = $this->getImdb("5011816");
-        $this->assertEquals(array('start' => 2015, 'end' => 0), $imdb->yearspan());
+        $this->assertEquals(array('start' => 2015, 'end' => 2019), $imdb->yearspan());
     }
 
     public function testYearspan()
