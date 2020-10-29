24f81f02cba7606b70db96af1d3dbd4e0db0e39e
Tom Boothman <tboothman@users.noreply.github.com>
2020-10-29 16:40:08 +0000

    Release 6.4.1

--- src/Imdb/MdbBase.php
+++ src/Imdb/MdbBase.php
@@ -21,7 +21,7 @@ use Psr\SimpleCache\CacheInterface;
  */
 class MdbBase extends Config
 {
-    public $version = '6.4.0';
+    public $version = '6.4.1';
 
     protected $months = array(
       "January" => "01",
