52da16fcf08eccee5ed78431c36174b5ac594638
Johan Eklund <hello@johaneklund.com>
2023-01-07 19:55:53 +0100

    Release 7.4.2

--- src/Imdb/MdbBase.php
+++ src/Imdb/MdbBase.php
@@ -22,7 +22,7 @@ use Psr\SimpleCache\CacheInterface;
  */
 class MdbBase extends Config
 {
-    public $version = '7.4.1';
+    public $version = '7.4.2';
 
     protected $months = array(
         "January" => "01",
