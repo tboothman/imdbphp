5ddf0e4671b639676041fb884c000af7a6ef341c
Johan Eklund <hello@johaneklund.com>
2023-01-07 01:28:54 +0100

    Don't use zip for PersonTest

--- tests/PersonTest.php
+++ tests/PersonTest.php
@@ -514,7 +514,7 @@ class PersonTest extends PHPUnit\Framework\TestCase
         $config->language = 'en-GB';
         $config->imdbsite = 'www.imdb.com';
         $config->cachedir = realpath(dirname(__FILE__) . '/cache') . '/';
-        $config->usezip = true;
+        $config->usezip = false;
         $config->cache_expire = 3600;
 
         return new \Imdb\Person($id, $config);
